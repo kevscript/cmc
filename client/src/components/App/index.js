01b6df08ea17307593d77797dff614e04bdeb607
Kevin Ostafinski <kevinostafinski@gmail.com>
2019-03-06 20:15:56 +0100

    use static data for developement + added coins filtering methods

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -2,10 +2,48 @@ import React, { useState, useEffect } from 'react';
 
 import CoinCard from '../CoinCard'
 import './app.css'
+import data from '../../data'
 
 const App = () => {
 
-  const [coins, setCoins] = useState(null)
+  const [coins, setCoins] = useState(data)
+  const [inputName, setInputName] = useState('')
+
+  const handleNameInput = (e) => {
+    setInputName(e.target.value)
+    searchCoin(e.target.value)
+  }
+
+  const sortByName = () => {
+    let coinsCopy = [...coins]
+    let newCoins = coinsCopy.sort((a, b) => {
+      let nameA = a.name.toUpperCase()
+      let nameB = b.name.toUpperCase()
+      return nameA === nameB ? 0 : (nameA < nameB ? -1 : 1)
+    })
+    setCoins(newCoins)
+  }
+
+  const sortByRank = () => {
+    let coinsCopy = [...coins]
+    let newCoins = coinsCopy.sort((a, b) => {
+      return a.cmc_rank - b.cmc_rank
+    })
+    setCoins(newCoins)
+  }
+
+  const searchCoin = (value) => {
+    let coinsCopy = [...coins]
+
+    if (value !== '') {
+      let newCoins = coinsCopy.filter(coin => coin.name.toUpperCase().includes(value.toUpperCase()))
+      setCoins(newCoins)
+    }
+      
+    if (value === '') {
+      setCoins(data)
+    }
+  }
 
 
   const fetchCoins = async () => {
@@ -16,15 +54,24 @@ const App = () => {
     setCoins(result)
   }
 
-  useEffect(() => {
+  /*useEffect(() => {
     fetchCoins()
-  }, [])
+  }, [])*/
 
   return (
-    <div className="cards">
-      {coins && coins.map(coin => (
-        <CoinCard coin={coin}/>
-      ))}
+    <div>
+
+      <div className="header">
+        <label htmlFor="coin-name">Search A Coin</label>
+        <input type="text" name="coin-name" onChange={handleNameInput} value={inputName}/>
+      </div>
+      <div className="cards">
+        {coins && coins.map(coin => (
+          <CoinCard coin={coin} key={coin.id}/>
+        ))}
+      </div>
+      <button onClick={sortByName}>sort by name</button>
+      <button onClick={sortByRank}>sort by rank</button>
     </div>
   )
 }
--- client/src/components/CoinCard/index.js
+++ client/src/components/CoinCard/index.js
@@ -3,15 +3,18 @@ import React from 'react'
 import './coincard.css'
 
 const CoinCard = ({coin}) => {
+  let date = new Date(coin.date_added)
   return (
-    <ul className="card">
-      <li>name: {coin.name}</li>
-      <li>id: {coin.id}</li>
-      <li>symbol: {coin.symbol}</li>
-      <li>slug: {coin.slug}</li>
-      <li>date_added: {coin.date_added}</li>
-      <li>cmc_rank: {coin.cmc_rank}</li>
-    </ul>
+    <div className="card">
+      <ul>
+        <li>name: {coin.name}</li>
+        <li>id: {coin.id}</li>
+        <li>symbol: {coin.symbol}</li>
+        <li>slug: {coin.slug}</li>
+        <li>date_added: {date.toDateString()}</li>
+        <li>cmc_rank: {coin.cmc_rank}</li>
+      </ul>
+    </div>
   )
 }
 
